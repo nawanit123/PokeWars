f6fcf53e5205b508249203558b28a88581be9e24
Nawanit <sharma.nawanit123@gmail.com>
2020-08-30 19:08:47 +0530

    Updates

---
+++ precache-manifest.8adf4a73a3d8723aa325bf5cb81b81e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e6588298542732d6730378b40a44994",
+    "url": "/pokewars/index.html"
+  },
+  {
+    "revision": "29810922684ed079fe91",
+    "url": "/pokewars/static/css/main.aae2667f.chunk.css"
+  },
+  {
+    "revision": "f7deda8ebda8ebd1bde0",
+    "url": "/pokewars/static/js/2.25b5c227.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokewars/static/js/2.25b5c227.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29810922684ed079fe91",
+    "url": "/pokewars/static/js/main.154ae720.chunk.js"
+  },
+  {
+    "revision": "bf5a655a463a75a4a776",
+    "url": "/pokewars/static/js/runtime-main.bce0813b.js"
+  }
+]);
